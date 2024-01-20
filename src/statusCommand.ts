b19543a9cf9d4fe800b5a1feb23f5f1e135b49cb
Jouni Alanen <jouni@diory.me>
2024-01-20 22:06:01 +0200

    statusCommand

--- src/statusCommand.ts
+++ src/statusCommand.ts
@@ -1,3 +1,14 @@
-const statusCommand = () => {}
+import { connectionInFocusId, roomInFocusId } from './configManager.js'
+
+const statusCommand = () => {
+  console.log(generateOutput())
+}
+
+// TODO: Generate table output with column headers
+const generateOutput = () => {
+  return [`roomInFocus: ${roomInFocusId()}`, `connectionInFocus: ${connectionInFocusId()}`].join(
+    '\n',
+  )
+}
 
 export { statusCommand }
