f3d5267207ad35e21632ec949d976694a15cc47e
Jouni Alanen <jouni@diory.me>
2024-01-21 10:25:59 +0200

    Drafting createConnectionCommand

--- src/connectionCommand.ts
+++ src/connectionCommand.ts
@@ -1,6 +1,8 @@
 import chalk from 'chalk'
+import { Connection, Room } from '@diograph/diograph'
+import { getClientAndVerify } from './createRoom.js'
 
-const connectionCommand = (commandName: string) => {
+const connectionCommand = (commandName: string, arg1: any, arg2: any) => {
   const validCommands = ['create', 'remove', 'delete', 'focus', 'listContents']
 
   if (!validCommands.includes(commandName)) {
@@ -14,7 +16,7 @@ const connectionCommand = (commandName: string) => {
 
   switch (commandName) {
     case 'create':
-      // Handle 'create' command
+      createConnectionCommand(arg1, arg2)
       break
     case 'remove':
       // Handle 'remove' command
@@ -29,6 +31,35 @@ const connectionCommand = (commandName: string) => {
     default:
       break
   }
+}
+
+const createConnectionCommand = async (
+  connectionAddress: string = process.cwd(),
+  contentClientType: string = 'LocalClient',
+) => {
+  // QUESTION: How to get Room object for roomInFocus?
+
+  // roomInFocusId()
+
+  // if (roomInFocus.connections.find((connection) => connectionAddress == connection.address)) {
+  //   console.error(chalk.red(`createRoom error: Room with address ${roomAddress} already exists`))
+  //   process.exit(1)
+  // }
+
+  // const connection = await createConnection(roomInFocus, connectionAddress, contentClientType)
+  // await addConnection(connectionAddress, contentClientType)
+  // await setConnectionInFocus(connection)
+
+  return
+}
+
+export const createConnection = async (room: Room, address: string, contentClientType: string) => {
+  const connectionClient = await getClientAndVerify(address, contentClientType)
+  const connection = new Connection(connectionClient)
+  room.addConnection(connection)
+  await room.saveRoom()
+
+  return connection
 }
 
 export { connectionCommand }
