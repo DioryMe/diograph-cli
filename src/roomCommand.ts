86dc8ed1e82e227cb316fd4a743c90944e786c70
Jouni Alanen <jouni@diory.me>
2024-03-16 16:36:39 +0200

    S3 works with room commands add & create
    - getAvailableClients() to read credentials from .dcli
    - pass available clients to createRoom and addRoom actions
    - fix clientType options for the commands

--- src/roomCommand.ts
+++ src/roomCommand.ts
@@ -1,9 +1,26 @@
 import { program } from 'commander'
 import { setRoomInFocus } from './appCommands/setInFocus.js'
-import { addRoom, constructAndLoadRoom, listRooms } from './configManager.js'
+import { addRoom, constructAndLoadRoom, getS3Credentials, listRooms } from './configManager.js'
 import { createRoom } from './createRoom.js'
 import chalk from 'chalk'
 import { LocalClient } from '@diograph/local-client'
+import { S3Client } from '@diograph/s3-client'
+// TODO: Fix to '@diograph/diograph/types' (works in S3Client, at least ConnectionClient)
+import { ConnectionClientList } from '@diograph/diograph/types.js'
+
+const getAvailableClients = async (): Promise<ConnectionClientList> => {
+  const credentials = await getS3Credentials()
+
+  return {
+    LocalClient: {
+      clientConstructor: LocalClient,
+    },
+    S3Client: {
+      clientConstructor: S3Client,
+      credentials: { region: 'eu-west-1', credentials },
+    },
+  }
+}
 
 const exitIfRoomAlreadyExists = async (roomAddress: string, method?: string) => {
   const roomList = await listRooms()
@@ -36,7 +53,8 @@ const createAction = async (options: createActionOptions) => {
   await exitIfRoomAlreadyExists(roomAddress, 'createRoom')
 
   try {
-    const room = await createRoom(roomAddress, contentClientType)
+    const availableClients = await getAvailableClients()
+    const room = await createRoom(roomAddress, contentClientType, availableClients)
     await addRoom(roomAddress, contentClientType)
     await setRoomInFocus(room)
   } catch (error) {
@@ -59,15 +77,8 @@ const addAction = async (options: createActionOptions) => {
   await exitIfRoomAlreadyExists(roomAddress, 'addRoom')
 
   try {
-    const room = await constructAndLoadRoom(roomAddress, contentClientType, {
-      LocalClient: {
-        clientConstructor: LocalClient,
-      },
-      // S3Client: {
-      //   clientConstructor: S3Client,
-      //   credentials: { region: 'eu-west-1', credentials },
-      // },
-    })
+    const availableClients = await getAvailableClients()
+    const room = await constructAndLoadRoom(roomAddress, contentClientType, availableClients)
     await addRoom(roomAddress, contentClientType)
     await setRoomInFocus(room)
   } catch (error) {
@@ -82,14 +93,14 @@ const createRoomCommand = program
   .command('create')
   .option('--path <value>', 'Create room to given path')
   .option('--here', 'Create room to current directory')
-  .option('--clientType', 'Set clientType (default: LocalClient)')
+  .option('--clientType <value>', 'Set clientType (default: LocalClient)')
   .action(createAction)
 
 const addRoomCommand = program
   .command('add')
   .option('--path <value>', 'Add room from given path')
   .option('--here', 'Add room from current directory')
-  .option('--clientType', 'Set clientType (default: LocalClient)')
+  .option('--clientType <value>', 'Set clientType (default: LocalClient)')
   .action(addAction)
 
 export { createRoomCommand, addRoomCommand }
