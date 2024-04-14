09d853025394b6498bb4d96e6925c8b9c05b0938
Jouni Alanen <jouni@diory.me>
2024-04-14 16:37:59 +0300

    Inject RoomConfig from .dcli to the diograph-server

--- src/startCommand.ts
+++ src/startCommand.ts
@@ -1,23 +1,11 @@
 import { program } from 'commander'
 import { ConfigClient, RoomConfig, bootstrap } from 'diograph-server'
+import { listRooms } from './configManager.js'
 
 const startAction = async () => {
   const configClient: ConfigClient = {
     getRoomConfig: async (roomId: string) => {
-      const rooms: { [roomId: string]: RoomConfig } = {
-        'room-1': {
-          address: '/Users/Jouni/Code/demo-content-room',
-          clientType: 'LocalClient',
-        },
-        'room-2': {
-          address: '/tmp/demo-content/room-1',
-          clientType: 'LocalClient',
-        },
-        'room-3': {
-          address: 'jvalanen-diory-test3/room',
-          clientType: 'S3Client',
-        },
-      }
+      const rooms = await listRooms()
       return rooms[roomId]
     },
   }
