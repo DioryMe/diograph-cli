0f94a0f091b3c453851b90abdd35608c4010136a
Jouni Alanen <jouni@diory.me>
2024-06-03 08:56:51 +0300

    Test for linkAction + fine tuning

--- src/dioryCommand.test.ts
+++ src/dioryCommand.test.ts
@@ -1,11 +1,16 @@
 import { Room } from '@diograph/diograph'
 import { roomInFocus } from './configManager.js'
-import { createAction } from './dioryCommand.js'
+import { createAction, linkAction } from './dioryCommand.js'
 import { describe, expect, it, mock, spyOn } from 'bun:test'
 
+const demoContentDiograph = require('../tests/demo-content-room/diograph.json')
+// TODO: Make launch.json to debug this
+
 const mockRoom = new Room()
+mockRoom.diograph.initialise(demoContentDiograph)
 mockRoom.saveRoom = mock()
 spyOn(mockRoom.diograph, 'addDiory')
+spyOn(mockRoom.diograph, 'addDioryLink')
 
 mock.module('./configManager', () => {
   return {
@@ -26,5 +31,20 @@ describe('createAction', () => {
       id: expect.any(String),
       text: mockText,
     })
+
+    // TODO: Verify that the diory was actually created
+    expect(Object.keys(mockRoom.diograph)).toHaveLength(14)
+  })
+})
+
+describe('linkAction', () => {
+  it('links two diories', async () => {
+    await linkAction('fromId', 'toId')
+
+    // Assert
+    expect(roomInFocus).toHaveBeenCalled()
+    expect(mockRoom.diograph.addDioryLink).toHaveBeenCalledWith({ id: 'fromId' }, { id: 'toId' })
+
+    // TODO: Verify that the diories were actually linked
   })
 })
