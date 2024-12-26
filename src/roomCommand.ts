d991465cafd23e5d7cb44b5d18038d5b963cba64
Jouni Alanen <jouni@diory.me>
2024-12-26 14:55:19 +0200

    dcli room remove & dcli room destroy
    - prompt: are you sure?
    - removeRoom to configManager

--- src/roomCommand.ts
+++ src/roomCommand.ts
@@ -1,6 +1,6 @@
 import { Command } from 'commander'
 import { setRoomInFocus } from './utils/setInFocus.js'
-import { addRoom, listRooms } from './utils/configManager.js'
+import { addRoom, listRooms, removeRoom } from './utils/configManager.js'
 import chalk from 'chalk'
 import { getAvailableClients } from './utils/getAvailableClients.js'
 import {
@@ -9,6 +9,28 @@ import {
   getClientAndVerify,
 } from '@diograph/diograph'
 import { ConnectionClientList } from '@diograph/diograph/types'
+import * as readline from 'readline'
+
+const confirmContinue = (question: string) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  })
+
+  return new Promise((resolve) => {
+    rl.question(question, (input: string) => {
+      const userInput = input.trim().toLowerCase()
+
+      if (userInput === 'y') {
+        rl.close()
+        resolve(true)
+        return
+      }
+      rl.close()
+      process.exit(0)
+    })
+  })
+}
 
 const exitIfAddressNotExists = async (
   address: string,
@@ -41,11 +63,7 @@ const getNativeConnectionAddress = (roomAddress: string) => {
 const exitIfRoomAlreadyExists = async (roomAddress: string, method?: string) => {
   const roomList = await listRooms()
 
-  if (
-    Object.values(roomList)
-      .map((r) => r.address)
-      .find((existingRoomAddress) => existingRoomAddress === roomAddress)
-  ) {
+  if (Object.values(roomList).find((roomConfig) => roomConfig.address === roomAddress)) {
     console.error(chalk.red(`${method} error: Room with address ${roomAddress} already exists`))
     process.exit(1)
   }
@@ -69,7 +87,10 @@ const createAction = async (options: createActionOptions) => {
 
   // Check if addresses exist
   // TODO: Prompt user to create address if it doesn't exist
-  // - needs to be done with client but client is not available here and doesn't have proper methods
+  // - not automatically created as otherwise we couldn't know if creating it would happen on purpose
+  // - needs to be done with client but ConnectionClient interface doesn't have proper methods
+  // - need extending with e.g. "createAddressForFolder" (+ implementing it to each client)
+  // - prompting "Are your sure to add new folder?" on each command using it
   const availableClients = await getAvailableClients()
   await exitIfAddressNotExists(roomAddress, contentClientType, availableClients, 'createRoom')
   await exitIfAddressNotExists(
@@ -125,6 +146,35 @@ const addAction = async (options: createActionOptions) => {
   console.log(chalk.green('Room added.'))
 }
 
+interface removeActionOptions {
+  roomId?: string
+  address?: string
+}
+
+const removeAction = async (options: removeActionOptions) => {
+  if (Object.keys(options).length === 0) {
+    console.log(chalk.red('Please provide a room --address or --id'))
+    process.exitCode = 1
+    return
+  }
+
+  const roomList = await listRooms()
+
+  const roomConfig = options.roomId
+    ? roomList[options.roomId]
+    : Object.values(roomList).find((roomConfig) => roomConfig.address === options.address)
+
+  if (!roomConfig || !(roomConfig && roomConfig.id)) {
+    console.error(chalk.red(`removeAction error: Room with address ${options.address} not found`))
+    process.exitCode = 1
+    return
+  }
+  const roomAddress = roomConfig.address
+  await confirmContinue(`Are you sure you want to remove room in ${roomAddress}? (y/n): `)
+
+  await removeRoom(roomConfig.id)
+}
+
 const focusAction = async (roomId: string) => {
   const roomConfig = (await listRooms())[roomId]
 
@@ -139,6 +189,56 @@ const focusAction = async (roomId: string) => {
   console.log(chalk.green('Room added.'))
 }
 
+interface destroyActionOptions {
+  roomId?: string
+  address?: string
+}
+
+const destroyAction = async (options: destroyActionOptions) => {
+  if (Object.keys(options).length === 0) {
+    console.log(chalk.red('Please provide a room --address or --id'))
+    process.exitCode = 1
+    return
+  }
+
+  const roomList = await listRooms()
+
+  const roomConfig = options.roomId
+    ? roomList[options.roomId]
+    : Object.values(roomList).find((roomConfig) => roomConfig.address === options.address)
+
+  if (!roomConfig || !(roomConfig && roomConfig.id)) {
+    console.error(
+      chalk.red(
+        `destroyRoom error: Room with address '${options.address}' or id '${options.roomId}' not found`,
+      ),
+    )
+    process.exitCode = 1
+    return
+  }
+  await confirmContinue(`Are you sure you want to destroy room in ${roomConfig.address}? (y/n): `)
+  const availableClients = await getAvailableClients()
+  const room = await constructAndLoadRoom(
+    roomConfig.address,
+    roomConfig.clientType,
+    availableClients,
+  )
+
+  try {
+    await Promise.all(
+      room.connections.map(async (connection) => {
+        connection.deleteConnection()
+      }),
+    )
+    await room.deleteRoom()
+    await removeRoom(roomConfig.id)
+  } catch (error) {
+    console.error(chalk.red(`destroyRoom error: ${error}`))
+    process.exitCode = 1
+    return
+  }
+}
+
 const createRoomCommand = new Command('create')
   .option('--address <value>', 'Create room to given address')
   .option('--here', 'Create room to current directory')
@@ -151,16 +251,26 @@ const addRoomCommand = new Command('add')
   .option('--clientType <value>', 'Set clientType (default: LocalClient)')
   .action(addAction)
 
+const removeRoomCommand = new Command('remove') //
+  .option('--address <value>', 'Destroy room from given address')
+  .option('--roomId <value>', 'Destroy room from given id')
+  .action(removeAction)
+
 const focusRoomCommand = new Command('focus') //
   .arguments('<roomId>')
   .action(focusAction)
+
+const destroyRoomCommand = new Command('destroy') //
+  .option('--address <value>', 'Destroy room from given address')
+  .option('--roomId <value>', 'Destroy room from given id')
+  .action(destroyAction)
 
 const roomCommand = new Command('room')
   .description('Manage rooms')
   .addCommand(createRoomCommand)
   .addCommand(addRoomCommand)
+  .addCommand(removeRoomCommand)
   .addCommand(focusRoomCommand)
-// .option('remove', 'Remove a room (arg1: roomAddress)')
-// .option('delete', 'Delete a room (arg1: roomAddress)')
+  .addCommand(destroyRoomCommand)
 
 export { roomCommand, createAction }
